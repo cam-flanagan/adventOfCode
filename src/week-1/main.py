8d37b9b75cf348af16b9e3131cfbab7aba3a9a47
Cameron Flanagan <cam.flanagan15@gmail.com>
2022-12-20 16:13:39 +0000

    week 1

---
+++ src/week-1/main.py
@@ -0,0 +1,21 @@
+
+def get_lines(filename: str) -> list:
+    input_file = open(filename, "r")
+    lines = input_file.readlines()
+    return [line.strip() for line in lines]
+
+def calculate_max_calories(lines: list) -> int:
+    elfs_calories = 0
+    elf_calorie_list = []
+
+    for line in lines: 
+        if line != '':
+            elfs_calories += int(line)
+        else:
+            elf_calorie_list.append(elfs_calories)
+            elfs_calories = 0
+    return max(elf_calorie_list)
+
+if __name__ == "__main__":
+    lines = get_lines("input.txt")
+    print(calculate_max_calories(lines))
